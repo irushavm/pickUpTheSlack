1b3997e2a45b9f3c5b6da89cff74355d04a69f3f
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2016-03-25 15:29:49 -0400

    Added server-client app and client-only app

---
+++ clientOnly/index.js
@@ -0,0 +1,99 @@
+'use strict';
+var os = require('os');
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+var Player = require('player');
+var config = require('../config.json');
+var AUDIO_FILE_LOCATION = '../public/audio/hello.mp3';
+
+var PORT = process.env.PORT || 5000;
+
+function handleRequest(request, response){
+    try {
+        console.log(request.url);
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+    console.log('Server listening on: http://localhost:%s', PORT);
+});
+
+dispatcher.onGet('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({status:'online',timeStamp:new Date()}));
+});
+
+dispatcher.onPost('/ping', function(req, res) {
+  console.log(req.params);
+  if(req.params.token === config.slackValidationToken) {
+    if(os.type()==='Windows_NT') {
+      var edge = require('edge');
+      var windowsPlayer = edge.func(function() {/*
+          async (input) => {
+              var player = new System.Media.SoundPlayer((string)input);
+              return new {
+                  start = (Func<object,Task<object>>)(async (i) => {
+                      player.Play();
+                      return null;
+                  }),
+                  stop = (Func<object,Task<object>>)(async (i) => {
+                      player.Stop();
+                      return null;
+                  })
+              };
+          }
+      */});
+      var winPlayer = windowsPlayer(AUDIO_FILE_LOCATION, true);
+      winPlayer.start(null, function (err) {
+          if (err) throw err;
+          console.log('Started playing');
+          res.writeHead(200);
+          res.end('Office Ping\'d!');
+      });
+
+      setTimeout(function () {
+          winPlayer.stop(null, function(err) {
+              if (err) throw err;
+              console.log('Stopped playing');
+          });
+      }, 25000);
+
+    }else {
+      var player = new Player(AUDIO_FILE_LOCATION);
+      player.play();
+
+      player.on('playing',function(item){
+        console.log('Pinging office!');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+          'response_type': 'in_channel',
+          'text': 'Office Ping\'d!'
+        }));
+      });
+      
+      // event: on error
+      player.on('error', function(err){
+        if(err !== 'No next song was found'){
+          console.log('Unable to play track');
+          console.log(err);
+          res.writeHead(500);
+          res.end('Internal Server Error');
+        }
+      });
+
+    }
+
+  }else {
+    res.writeHead(401);
+    res.end('Unauthorized');
+  }
+});
+
+dispatcher.onError(function(req, res) {
+  res.writeHead(404);
+});
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 "use strict";
-var fs = require('fs');
 var http = require('http');
 var dispatcher = require('httpdispatcher');
-// var config = require('./config.json');
+var config = require('./config.json');
 
 var PORT = process.env.PORT || 5000;
 
@@ -21,39 +20,19 @@ server.listen(PORT, function(){
     console.log('Server listening on: http://localhost:%s', PORT);
 });
 
-dispatcher.onGet('/audio/hello.mp3', function (req,res){
-  console.log('Getting Audio');
-  console.log(req.url);
-  var path = __dirname + '/public' + req.url;
-   fs.stat(path, function(err, stat){
-       if( err ) {
-           res.writeHead(404, {'Content-Type': 'text/html'});
-           res.end(''+err);
-       } else {
-           res.writeHead(200, {
-               'Content-Type': 'audio',
-               'Content-Length': stat.size});
-           var stream = fs.createReadStream(path);
-           stream.pipe(res);
-       }
-   } );
-});
-
 dispatcher.onGet('/', function(req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify({status:'online',timeStamp:new Date()}));
 });
 
 dispatcher.onPost('/ping', function(req, res) {
-  console.log('HEADERS\n----------');
-  console.log(req.headers);
-  console.log('PARAMS\n----------');
-  console.log(req.params);
-  console.log('BODY\n----------');
-  console.log(req.body);
 
-  // res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('');
+  if(req.params.token === config.slackValidationToken) {
+    res.redirect(config.remoteURL+req.url);
+  }else {
+    res.writeHead(401);
+    res.end('Unauthorized');
+  }
 });
 
 dispatcher.onError(function(req, res) {
